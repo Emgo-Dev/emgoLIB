f38aacd049e4028bef1add690531c11c29ca85a3
Michael Goldspinner <emgo.development@gmail.com>
2017-12-03 19:14:43 -0500

    Code Maintanence
    Consisten format & writing style. Renamed some identifiers and removed comments hoping to let the code speak for itself.

--- js/DOM.js
+++ js/DOM.js
@@ -1,88 +1,96 @@
 var DOM = {
-  ele(selector) {
-    if (document.querySelectorAll(selector).length > 1) {
-      return document.querySelectorAll(selector);
-    } else {
-      return document.querySelector(selector);
-    }
-  },
+	ele( selector ){
+		if (document.querySelectorAll(selector).length > 1) {
+			return document.querySelectorAll(selector);
+		} else {
+			return document.querySelector(selector);
+		}
+	},
 
-  empty( element ){
-    element.innerHTML = null;
-  },
+	empty( element ){
+		element.innerHTML = null;
+	},
 
-  // Make HTML Element Node from Custom Object
-    /*
-    [
-      { // Each element is an object in this array (with these properties)
-        Type: "element_tag",
-        Attributes: [ // Each attribute is an object in this array
-          {
-            Name: "attibute_name",
-            Value: "attribute_value"
-          }
-        ],
-        Text: "text_node"
-      }
-    ]
-    */
+	// Make HTML Element Node from Custom Object
+		/*
+		[
+			{ // Each element is an object in this array (with these properties)
+				Type: "element_tag",
+				Attributes: [ // Each attribute is an object in this array
+					{
+						Name: "attibute_name",
+						Value: "attribute_value"
+					}
+				],
+				Text: "text_node"
+			}
+		]
+		*/
 
-  makeEle(ElementNodeList = null) {
-    // This function accepts an array of objects with properties representing an element node in the DOM
-    // Object properties should be
-    // Type - The type of element
-    // Attributes - An array of attribute objects in pairs of { Name,] Value,] }
-    // Text - The elements text node content
-    // Children - an array of children element objects
+	/**
+	 * Builds HTML Element Node from Object Schematics
+	 * @param 	{array}		objectSchematicCollection	Collection of object to use as schematics for
+	 *                                            		creating HTML elements. The object schematics'
+	 *                                            		properties represent aspects of an HTML Node.
+	 *                                            		
+	 *                                            		Schematics:
+	 *                                            		Type - Element Tag Name
+	 *                                             		Attributes - An array of attribute objects
+	 * 													Should use two properties, Name for attribute
+	 * 													name, and Value for attribute value
+	 * 													Text - Value for element text node
+	 * 													Children - Array of children element
+	 * 													represented by object schematics
+	 *
+	 * @return 	{array}									Collection of created HTML elements.
+	 */
+	makeEle( objectSchematicCollection = null ){
+		let elementNodeCollection = [];
 
-    // Collection of Elements
-    var el = [];
+		// Create Elements
+		for( schematic of objectSchematicCollection ){
+			elementNodeCollection.push( document.createElement(schematic.Type) );
 
-    // Create Elements
-    for( item of ElementNodeList ){
-      el.push( document.createElement(item.Type) );
+			schematic.Attributes.forEach( x => elementNodeCollection[elementNodeCollection.length-1].setAttribute(x.Name, x.Value) );
 
-      item.Attributes.forEach( a => el[el.length-1].setAttribute(a.Name, a.Value) );
+			elementNodeCollection[elementNodeCollection.length-1].innerText = schematic.Text;
 
-      el[el.length-1].innerText = item.Text;
+			if( typeof schematic.Children != "undefined" && schematic.Children.length > 0 ){
+				DOM.makeEle(schematic.Children).forEach( x => elementNodeCollection[elementNodeCollection.length-1].append( x ) );
+			}
+		}
 
-      if( typeof item.Children != "undefined" && item.Children.length > 0 ){
-        DOM.makeEle(item.Children).forEach( c => el[el.length-1].append( c ) )
-      }
-    }
+		// for( element of el ){ // console.log(element); // Debug }
 
-    for (element of el) {
-      // console.log(element); // Debug
-      return el;
-    }
-  },
+		return elementNodeCollection;
+	},
 
-  modifyTextNodeOf( element, value ){
-    element.innerHTML = value;
-  }
+	modifyTextNodeOf( element, value ){
+		element.innerHTML = value;
+	}
 }
 
 function domEle( cssSelector ) {
-  if (document.querySelectorAll(cssSelector).length > 1) {
-    return document.querySelectorAll(cssSelector);
-  } else {
-    return document.querySelector(cssSelector);
-  }
+	if (document.querySelectorAll(cssSelector).length > 1) {
+		return document.querySelectorAll(cssSelector);
+	} else {
+		return document.querySelector(cssSelector);
+	}
 }
 
 function domEleNext( elementNode ){ return elementNode.nextElementSibling; }
 function domElePrev( elementNode ){ return elementNode.previousElementSibling; }
 
 function domEleIndex(element) {
-  let step = -1;
+	let step = -1;
 
-  while( element ){
-    step += 1;
+	while( element ){
+		step += 1;
 
-    element = element.previousElementSibling;
-  }
+		element = element.previousElementSibling;
+	}
 
-  return step;
+	return step;
 }
 
 function domEleInto( elementNode, elementNodeTarget ){ elementNodeTarget.append(elementNode); }
@@ -90,17 +98,17 @@ function domEleAfter( elementNode, elementNodeTarget ){ elementNodeTarget.after(
 function domEleBefore( elementNode, elementNodeTarget ){ elementNodeTarget.before(elementNode); }
 
 function domEleSize( elementNode, measurement=null ){
-  switch (measurement) {
-    case "height": 
-      return elementNode.offsetHeight;
-      break;
-    case "width":
-      return elementNode.offsetWidth;
-      break;
-    default:
-      return [elementNode.offsetWidth, elementNode.offsetHeight];
-      break;
-  }
+	switch (measurement) {
+		case "height": 
+			return elementNode.offsetHeight;
+			break;
+		case "width":
+			return elementNode.offsetWidth;
+			break;
+		default:
+			return [elementNode.offsetWidth, elementNode.offsetHeight];
+			break;
+	}
 }
 
 function domEleClasses( elementNode ){ return elementNode.className.split(" "); }
@@ -108,15 +116,15 @@ function domEleClasses( elementNode ){ return elementNode.classList; }
 
 function domEleClass( elementNode, className ){ return elementNode.classList.contains( className ); }
 function domEleClass( elementNode, className ){
-  let classList = domEleClasses( elementNode );
+	let classList = domEleClasses( elementNode );
 
-  for( let a = 0; a <= classList.length; a++ ){
-    if( className === classList[a] ){
-      return true;
-    }
-  }
+	for( let a = 0; a <= classList.length; a++ ){
+		if( className === classList[a] ){
+			return true;
+		}
+	}
 
-  return false;
+	return false;
 }
 
 // @function          removeClass()
@@ -128,17 +136,17 @@ function domEleClass( elementNode, className ){
 // $measure [string]  A regular expression to match for
 
 function domEleRemoveClass( elementNode, className ){
-  let classList = domEleClasses( elementNode );
-  let result = [];
-  
-  for( s = 0; s <= classList.length; s++ ){
-    if( classList[s] === className ){}
-    else{
-      result.push(classList[s]);
-    }
-  }
-  
-  return result.join(" ");
+	let classList = domEleClasses( elementNode );
+	let result = [];
+	
+	for( s = 0; s <= classList.length; s++ ){
+		if( classList[s] === className ){}
+		else{
+			result.push(classList[s]);
+		}
+	}
+	
+	return result.join(" ");
 }
 
 ////
@@ -147,25 +155,25 @@ function domEleRemoveClass( elementNode, className ){
 ////
 
 Object.prototype.prev = function(){
-  return this.previousElementSibling; }
+	return this.previousElementSibling; }
 Object.prototype.next = function(){
-  return this.nextElementSibling; }
+	return this.nextElementSibling; }
 Object.prototype.measure = function(measurement=null){
-  return [this.offsetHeight, this.offsetWidth]; }
+	return [this.offsetHeight, this.offsetWidth]; }
 Object.prototype.classes = function(){
-  return this.className.split(" "); };
+	return this.className.split(" "); };
 Object.prototype.hasClass = function(className){
-  var classList = this.classList != 0 ? this.classList : null;;
+	var classList = this.classList != 0 ? this.classList : null;;
 
-  if( classList !== null ){
-    for (var s=0; s<=this.classList.length; s++) {
-      if (classList[s] === className) {
-        return true;
-      } else { }
-    }
+	if( classList !== null ){
+		for (var s=0; s<=this.classList.length; s++) {
+			if (classList[s] === className) {
+				return true;
+			} else { }
+		}
 
-    return false;
-  }else{ return null; }
+		return false;
+	}else{ return null; }
 }
 
 ////
@@ -174,21 +182,21 @@ Object.prototype.hasClass = function(className){
 ////
 
 function ajaxREQ(type, url, post) {
-  let httpRequest = new XMLHttpRequest();
+	let httpRequest = new XMLHttpRequest();
 
-  if( !httpRequest ){ return false; }
+	if( !httpRequest ){ return false; }
 
-  httpRequest.onreadystatechange = ajaxget;
-  httpRequest.open(type, url);
-  httpRequest.send(post);
+	httpRequest.onreadystatechange = ajaxget;
+	httpRequest.open(type, url);
+	httpRequest.send(post);
 }
 
 function ajaxRET() {
-  if( httpRequest.readyState === XMLHttpRequest.DONE ){
-    if( httpRequest.status === 200 ){
-      return httpRequest.responseText;
-    }else{
-      alert("AJAX error.");
-    }
-  }
+	if( httpRequest.readyState === XMLHttpRequest.DONE ){
+		if( httpRequest.status === 200 ){
+			return httpRequest.responseText;
+		}else{
+			alert("AJAX error.");
+		}
+	}
 }
