a31944c4c78ea6311681eb07c6a629934b4002dc
Michael Goldspinner <emgo.development@gmail.com>
2017-12-05 18:50:35 -0500

    Cleanup for Calendar Object & Methods
    Decided what was really necessary for Calendar object. It's purpose was to supplement Date object, not replace it.
    
    The Date constructor didn't return, or even store Months in it. It would return the index of the Month. The Calendar does two things well so far, stores the months of a year and the # of days they have, and allows you to 'get' a month using a shorthand for the month name (ie: Oct).

--- js/library.js
+++ js/library.js
@@ -1,114 +1,35 @@
-/////////////////////////////////////////////////////////
-///                                                   ///
-///                  Emgo JS Library                  ///
-///                Michael Goldspinner                ///
-///                 Emgo Development                  ///
-///                    10/15/2017                     ///
-///                                                   ///
-///  ***********************************************  ///
-///                                                   ///
-///  Library contains general purpose functions and   ///
-///  classes.                                         ///
-///                                                   ///
-/////////////////////////////////////////////////////////
-///                                                   ///
-///                   Dependencies                    ///
-///                                                   ///
-///  ***********************************************  ///
-///                                                   ///
-///              Site Settings & Paths                ///
-///                                                   ///
-/////////////////////////////////////////////////////////
+////
+////////        Data
+////////////////////////////////////////////////
+////
 
+var Calendar = {
+  Month: ["january", "februrary", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"],
 
+  MonthLength: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
 
-
-
-function getArea(circle=false, radius=null, length, width, type) {
-    if (circle == false && (length != null || width != null)) {
-      return length * width + " " + type;
-    } else if (circle == true && radius != null) {
-      return Math.PI * radius * radius + type;
-    } else {
-      console.log("There was an error, could not return data.")
-      return false;
-    }
-}
-
-function getPerimeter(circle=false, radius=null, length, width, type) {
-    if (circle == false && (length != null || width != null)) {
-      return (length * 2) + (width * 2) + type;
-    } else if (circle == true && radius != null) {
-      return 2 * Math.PI * radius + type;
-    } else {
-      console.log("There was an error, could not return data.")
-      return false;
-    }
-}
-
-
-
-
-////////////////////////////////
-///                          ///
-///      States Object       ///
-///                          ///
-///  ----------------------  ///
-///                          ///
-///                          ///
-////////////////////////////////
-
-
-
-////////////////////////////////
-///                          ///
-///     Calendar Object      ///
-///                          ///
-///  ----------------------  ///
-///                          ///
-///  Calendar information    ///
-///  and handling of dates   ///
-///                          ///
-////////////////////////////////
-
-var emgoCAL = {
-  Months: [
-    ["January", "Februrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  Holidays: [
+    [10, 23, "Thanksgiving Day"],
+    [11, 25, "Christmas Eve"],
+    [11, 25, "Christmas Day"],
+    [11, 31, "New Years Eve"]
   ],
 
-  suffix(Date) {
-    Date = typeof Date === "string" && !isNaN(Date) ? parseInt(Date) : Date;
-    var single = Date % 10; // Single Digit
-    var tenth = Date % 100; // Both Digits
-    var suffs = [
-      [1, 2, 3, 4], // Pattern
-      ["st", "nd", "rd", "th"] // Suffixes
-    ];
+  getMonth( index ){
+    return this.Month[index]
+  },
 
-    if( suffs[0].includes(single) && (tenth < 11 || tenth > 13) ){
-      return tenth+suffs[1][single-1];
-    } else {
-      return tenth+suffs[1][3];
+  getMonthOf( shortMonth ){
+    for( var a = 0; a < this.Month.length; a++ ){
+      var match = 1;
+      if( shortMonth[0].toLowerCase() === this.Month[a][0] ){
+        for( var b = 1; b < 3; b++ ){
+          match+=1;
+          if( shortMonth[b].toLowerCase() !== this.Month[a][b] ){ break; }
+          else if( match === 3 ){ return this.Month[a]; }
+        }
+      }
     }
-
-    // // Is it a Single Digit?
-    // if( Date.length < 2 ){
-    //   return !suffs[0].includes(parseInt(Date)) ? Date.concat(suffs[1][3]) : Date.concat(suffs[1][Date-1]);
-    // }
-
-    // // Is it a Double Digit?
-    // else if( Date.length > 1 ){
-    //   // Is it a teen? (11, 12, 13)
-    //   if( parseInt(Date.slice(0,1)) === 1 ){
-    //     return Date.concat(suffs[1][3]);
-    //   }
-
-    //   // Does it match the Suffix Pattern?
-    //   else if( suffs[0].includes(parseInt(Date.slice(1,2))) ) {
-    //     return Date.concat(suffs[1][parseInt(Date.slice(1,2))-1]);
-    //   }
-    // }
   }
 }
 
