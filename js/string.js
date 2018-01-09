4d24e65f35437e27fc5674bc4e4f7c1637690255
Michael Goldspinner <emgo.development@gmail.com>
2018-01-08 20:17:13 -0500

    toSuffix()
    Refactored suffix() to ES6 spec. This version was submitted to 30 Seconds of Code (JS Repo) which was accepted.

--- js/string.js
+++ js/string.js
@@ -51,39 +51,10 @@ String.prototype.typeInto( elementNode ){
   }, 50);
 }
 
-suffix( Date ){
-	if( typeof Date !== "string" ){
-		Date = parseInt(Date);
-	}
+const toSuffix = int => {
+  int = parseInt(int);
+  var digits = [ (int % 10), (int % 100)];
+  var suffix = [[1, 2, 3, 4], ["st", "nd", "rd", "th"]];
 
-	var single = Date % 10; // Single Digit
-	var tenth = Date % 100; // Both Digits
-	var suffs = [
-	  [1, 2, 3, 4], // Pattern
-	  ["st", "nd", "rd", "th"] // Suffixes
-	];
-
-	if( suffs[0].includes(single) && (tenth < 11 || tenth > 13) ){
-	  return tenth+suffs[1][single-1];
-	} else {
-	  return tenth+suffs[1][3];
-	}
-
-	// [Old] Is it a Single Digit?
-    // if( Date.length < 2 ){
-    //   return !suffs[0].includes(parseInt(Date)) ? Date.concat(suffs[1][3]) : Date.concat(suffs[1][Date-1]);
-    // }
-
-    // // Is it a Double Digit?
-    // else if( Date.length > 1 ){
-    //   // Is it a teen? (11, 12, 13)
-    //   if( parseInt(Date.slice(0,1)) === 1 ){
-    //     return Date.concat(suffs[1][3]);
-    //   }
-
-    //   // Does it match the Suffix Pattern?
-    //   else if( suffs[0].includes(parseInt(Date.slice(1,2))) ) {
-    //     return Date.concat(suffs[1][parseInt(Date.slice(1,2))-1]);
-    //   }
-    // }
+  return suffix[0].includes(digits[0]) && (digits[1] < 11 || digits[1] > 13) ? int + suffix[1][digits[0]-1] : int + suffix[1][3];
 }
