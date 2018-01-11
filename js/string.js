7790a3917738339c0aae3203c4527e3fc74e24c5
Michael Goldspinner <emgo.development@gmail.com>
2018-01-10 19:31:32 -0500

    New Functions
    Wrote some new functions for formatting time and number strings.

--- js/string.js
+++ js/string.js
@@ -66,24 +66,54 @@ const toMeridiem12 = int => {
  * @param		{integer}	count		[The digit length to maintain.]
  * @return	{string}					[Formatted data. Example: "1" -> "01".]
  */
-const toDigits = ( integer=0, digitCount=1 ) => {
-	return integer.toString().padStart(digitCount, "0");
+const setDigits = ( int=0, len=1, dir=0 ) => {
+	return dir > 0 ? int.toString().padStart(len, "0") : int.toString().padEnd(len, "0");
 }
 
 /**
- * Formats a Time String
+ * Format Numbers
  * Maintains double digit format for time strings.
  * @param		{string}	time	[The time string.]
  * @return	{string}				[Formatted string (H:M:SS -> HH:MM:SS).]
  */
-const toTimeFormat = ( time ) => {
-	let times = time.split(":");
+const getColonTime = ( times )=>{
+	return times.map( t => setDigits(t, 2, 1) ).join(":");
+}
 
-	times.forEach( (x,y,z) => {
-		z[y] = toDigits(x, 2);
-	});
+const to12Hour = ( int ) => { return int === 0 || int === 12 || int === 24 ? 12 : int % 12; }
 
-	return times.join(":");
+/**
+ * Format Integer w/ Commas
+ * @param		{integer}	int	[The number to format]
+ * @return	{string}			[Formatted number]
+ */
+const getWrittenNumb = ( int ) => {
+	let number = int.toString();
+	const seperator = ",";
+	let length = number.length;
+	let readableInt = "";
+
+	for( let a = number.length-1, b=0; a >= 0; a--, b++ ){
+		// Read through number backwards (number[a--])
+		// Use modulo to find every 3rd digit, excluding 0 because 0%3 === 3%3
+		if( (b > 0) && ((b%3) === 0) ){ readableInt += seperator; }
+
+		readableInt += number[a];
+	}
+
+	return readableInt.split("").reverse().join("");
+}
+
+/**
+ * Real Price Format
+ * @param		{integer/float}	number	[A number to conver to a real price]
+ * @return	{string}								[description]
+ */
+const toWrittenPrice = ( number ) => { 
+	number = number.toString();
+	let parts = number.search(/[.]/g) > 0 ? number.split(".") : [number, ""];
+
+	return parts[0].concat("." + parts[1].padEnd(2, "0"));
 }
 
 /**
