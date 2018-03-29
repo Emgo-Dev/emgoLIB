bd0024e15db1358a71e5460714067ade09257c5a
Michael Goldspinner <emgo.development@gmail.com>
2018-03-29 14:34:49 -0400

    Code Maintanence
    Renaming and refactoring functions mostly in math.js.
    
    Combined area functions into one.

--- code/js/array.js
+++ code/js/array.js
@@ -1,4 +1,7 @@
-function toObject( values, keys ){
+/*
+  toObject(["a", "b"], ["dee", "dum"]) -> { a: "dee" b: "dum" }
+*/
+function toObject( keys, values ){
     let keyI = 0;
     let loopI = 0;
     let obj = {};
--- code/js/math.js
+++ code/js/math.js
@@ -3,65 +3,41 @@
 ////////////////////////////////////////////////
 ////
 
-function add( a, b ){
-	return a + b;
+function add(){
+	let s = 0;
+	for( let n in arguments ){ s += arguments[n]; }
+	return s;
 }
 
-function subtract( a, b ){
-	return a - b;
+function subtract(){
+	let s = arguments[0];
+	for( let n in arguments ) if( n > 0 ) arguments[n] < 0 ? s += arguments[n] : s-= arguments[n];
+	return s;
 }
 
-function multiply( a, b ){
-	return a * b;
+function product(){
+	let p = 1;
+	for( let n in arguments ) p *= arguments[n];
 }
 
-function divide( a, b ){
-	return a / b;
+function divide(){
+	let d = 1;
+	for( let n in arguments ) d /= arguments[n];
 }
 
-function sum( numberCollection ){
-	let sumTotal = 0;
-
-	numberCollection.forEach((x)=>{ sumTotal += x; })
-
-	return sumTotal;
-}
-
-function sumOf( numberCollection ){
-	return sum( numberCollection );
-}
-
-function product( numberCollection ){
-	let productTotal = 1; // Setting to 0 would cancel out any operation using *= operator
-
-	numberCollection.forEach((x)=>{ productTotal *= x; })
-
-	return productTotal;
-}
-
-function productOf( numberCollection ){
-	product( numberCollection );
-}
-
-function getIntBetween( a, b ){
+function countBetween( a, b ){
 	let c = [];
-	
+
 	while( a <= b ){
 		c.push(a);
 		a++;
 	}
-	
+
 	return c;
 }
 
-/**
- * https://github.com/Chalarangelo/30-seconds-of-code/pull/543/commits/3c69263268f988937ea7041b5441d29f373416fd
- * @param  {integer} len Iteration count
- * @param  {integer} min Lowest possible integer
- * @param  {integer} max Highest possible integer
- * @return {array}       A collection of all generated results
- */
-const randomIntWithLengthInRange = (len, min, max) =>
+// https://github.com/Chalarangelo/30-seconds-of-code/pull/543/commits/3c69263268f988937ea7041b5441d29f373416fd
+const randomBetween = (len, min, max) =>
 	Array.from({ length: len }, () =>
 		Math.floor(Math.random() * (max - min) + min)
 	);
@@ -71,40 +47,29 @@ const randomIntWithLengthInRange = (len, min, max) =>
 ////////////////////////////////////////////////
 //// http://www.mathsisfun.com/area.html
 
-function areaOfCircle( radius ){
-	return Math.PI * radius * radius;
+function area( type, dimensions ){
+	const w = arguments[1];
+	const h = arguments[2];
+	const w2 = arguments[3];
+	const areas = {
+		circle( r ){ return Math.PI * r * r; },
+		ellipse( w, h ){ return Math.PI * w * h; },
+		parallelogram( w, h ){ return w * h; },
+		rectangle( w, h ){ return w * h; },
+		square( i ){ return i * i; },
+		trapezoid( w1, h, w2, ){ return w1 * w2 * h; },
+		triangle( w, h ){ return w * h; }
+	}
+
+	return areas[type](w, h, w2);
 }
 
-function areaOfEllipse( width, height ){
-	return Math.PI * width * height;
+function circumference( r ){
+	return Math.PI * r * r * 2;
 }
 
-function areaOfParallelogram( width, height ){
-	return width * height;
-}
-
-function areaOfRectangle( width, height ){
-	return width * height;
-}
-
-function areaOfSquare( side ){
-	return side * side;
-}
-
-function areaOfTrapezoid( width1, width2, height ){
-	return ((width1 + width2) * height) / 2;
-}
-
-function areaOfTriangle( width, height ){
-	return width * height / 2;
-}
-
-function circumferenceOfCircle( radius ){
-	return areaOfCircle( radius ) * 2;
-}
-
-function getPerimiter( measurements ){
-	let perimiter = 0;
-
-	measurements.forEach((x)=>{ perimiter += x; });
+function perimeter(){
+	let p = 0;
+	for( m in arguments ){ p += arguments[m];	}
+	return p;
 }
