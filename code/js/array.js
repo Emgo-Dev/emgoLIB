e3f775fb4fe084ac56d3aeabf76ed221d3db26a4
Michael Goldspinner <emgo.development@gmail.com>
2018-04-05 00:35:27 -0400

    Improve sift()
    array function sift() can now handle nested collections. Previously, sift() only iterated over the first tier of the collection, now, it checks if each value is an array or object using the __proto__.constructor.name property. If so, it recalls a new instance of sift() added onto the top tier sifted array collection

--- code/js/array.js
+++ code/js/array.js
@@ -19,10 +19,10 @@ function toObject( keys, values ){
             keyI += 1;
         }else{
             obj[keys[loopI]] = val;
-        }
+    }
 
         loopI += 1;
-    }
+  }
 
     return obj;
 }
@@ -32,6 +32,34 @@ Array.prototype.toObject = function( keys ){
     let loopI = 0;
     let obj = {};
     for( val of this ){
+function sift( col = [], sifted = [], i = -1 ){
+  i = i + 1;
+  if( !["Array", "Object"].includes(col["__proto__"]["constructor"]["name"]) ){ throw TypeError(`sift() requires first parameter to be a type of collection (Array, Array-like, or Object). The constructor of given data was ${col["__proto__"]["constructor"]["name"]} and data type is ${typeof col}`); }
+
+  if( col[i] === undefined ){
+    if( ["Object"].includes(col["__proto__"]["constructor"]["name"]) ){
+      for( let p in col ){
+        if( isNaN(p) && [undefined].includes(col["__proto__"][p]) ){
+          if( ["Object"].includes(col[p]["__proto__"]["constructor"]["name"]) ){
+            sifted[sifted.length] = sift(col[p]);
+          }else{
+            sifted[sifted.length] = col[p];
+          }
+        }
+      }
+    }
+    return sifted;
+  }else{
+    if( ["Array", "Object"].includes(col[i]["__proto__"]["constructor"]["name"]) ) return sift( col, [...sifted, sift(col[i])], i);
+    return sift( col, [...sifted, col[i]], i );
+  }
+}
+
+/*
+  toObject(["a", "b"], ["dee", "dum"]) -> { a: "dee" b: "dum" }
+*/
+function toObject( keys, values ){
+    for( let val of values ){
         let noKey = !keys[loopI] ? true : false;
         if( noKey ){
             obj[keyI] = val;
