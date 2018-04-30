b2a0b5401a6172c91119f39b30f6bd2d87098023
Michael Goldspinner <emgo.development@gmail.com>
2018-04-30 10:08:27 -0400

    Code Maintanence: sift()
    Tidying up and improving code of sift() and it's documentation use cases.

--- code/js/array.js
+++ code/js/array.js
@@ -13,37 +13,60 @@ function isArray( col ){
   return true;
 }
 
-  }
-
-    return obj;
-}
-
-Array.prototype.toObject = function( keys ){
-    let keyI = 0;
-    let loopI = 0;
-    let obj = {};
-    for( val of this ){
-function sift( col = [], sifted = [], i = -1 ){
-  i = i + 1;
-  if( !["Array", "Object"].includes(col["__proto__"]["constructor"]["name"]) ){ throw TypeError(`sift() requires first parameter to be a type of collection (Array, Array-like, or Object). The constructor of given data was ${col["__proto__"]["constructor"]["name"]} and data type is ${typeof col}`); }
-
-  if( col[i] === undefined ){
-    if( ["Object"].includes(col["__proto__"]["constructor"]["name"]) ){
-      for( let p in col ){
-        if( isNaN(p) && [undefined].includes(col["__proto__"][p]) ){
-          if( ["Object"].includes(col[p]["__proto__"]["constructor"]["name"]) ){
-            sifted[sifted.length] = sift(col[p]);
-          }else{
-            sifted[sifted.length] = col[p];
-          }
+/*
+  ARRAY FOR TESTING AND DEVELOPMENT
+  [
+    1,
+    2,
+    3,
+    [
+      4,
+      5,
+      6,
+      [
+        "end of array"
+      ]
+    ],
+    {
+      "0": "Mike",
+      "1": "G",
+      "a": "A",
+      "b": {
+        "0": "Test",
+        "a": "a",
+        "b": {
+          "0": "fail",
+          "test": "failure"
         }
       }
     }
+  ]
+
+  sift([ 1, 2, 3 ]) -> [1, 2, 3]
+  sift([ 1, [2, 3] ]) -> [ 1, [2, 3] ]
+  sift([ 1, [ 2, [3, 4, 5] ] ]) -> [ 1, [ 2, [3, 4, 5] ] ]
+  sift([ 1, [ 2, {0: 3, a: 4, b: 5, 1: 6} ] ]) -> [ 1, [ 2, [3, 6, 4, 5] ] ]
+  sift({ 0: 0, 1: 1, 2: 2 }) -> [0, 1, 2]
+  sift({ a: 0, 0: 1, x: 2 }) -> [1, 0, 2]
+  sift({ a: 0, 5: 1, x: 2 }) -x [0, 2]
+*/
+function sift( col = [], sifted = [], i = -1 ){
+  i = i + 1;
+  if( !["Array", "Object"].includes(col["__proto__"]["constructor"]["name"]) ){ throw TypeError(`sift() requires first parameter to be a type of collection (Array, Array-like, or Object). The constructor of given data was ${col["__proto__"]["constructor"]["name"]} and data type is ${typeof col}`); }
+  let numInEnd = [undefined].includes(col[i]), colIsObj = ["Object"].includes(col["__proto__"]["constructor"]["name"]);
+  let isPropObj = ( obj, prop ) => ["Object"].includes(obj[prop]["__proto__"]["constructor"]["name"]);
+  let isPropVendor = ( obj, prop ) => isNaN(prop) && [undefined].includes(obj["__proto__"][prop]) ? true : false;
+
+  if( numInEnd ){
+    if( colIsObj ){
+      for( let p in col ) if( isPropVendor(col, p) ) if( isPropObj(col, p) ) sifted[sifted.length] = sift(col[p]); else sifted[sifted.length] = col[p];
+    }
+
     return sifted;
-  }else{
-    if( ["Array", "Object"].includes(col[i]["__proto__"]["constructor"]["name"]) ) return sift( col, [...sifted, sift(col[i])], i);
-    return sift( col, [...sifted, col[i]], i );
   }
+
+  if( ["Array", "Object"].includes(col[i]["__proto__"]["constructor"]["name"]) ) return sift( col, [...sifted, sift(col[i])], i);
+  return sift( col, [...sifted, col[i]], i );
 }
 
 /*
