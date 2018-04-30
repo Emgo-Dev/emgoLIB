8eef33eff650ed5f169d3ca6a36a15deca869de9
Michael Goldspinner <emgo.development@gmail.com>
2018-04-30 12:47:49 -0400

    Refactoring getDecimals()
    Improving function to handle more fail cases. Only returns decimals after the last occurrence of a ".". No longer uses String or Array methods, loops through string using conditional checks.

--- code/js/string.js
+++ code/js/string.js
@@ -177,11 +177,21 @@ getDecimals("10.500") -> "500"
 getDecimals("10..500") -x ".500"
 getDecimals("10...500") -x "..500"
 */
-function getDecimals( str ){
-    if( !["string", "number"].includes(typeof str) ){ throw TypeError(`toWrittenNumb() requires first parameter to be a string or number. The data type of given value was ${typeof str}.`); }
-    if( typeof str == "number"){ str = String(str % 1).slice(2); }
+function getDecimals( str = "" ){
+  if( !["string", "number"].includes(typeof str) ){ throw TypeError(`toWrittenNumb() requires first parameter to be a string or number. The data type of given value was ${typeof str}.`); }
 
-    return str.slice(str.search("\\.") + 1);
+  let decimal = "";
+  let foundDecimal = false;
+  for( let i = 0; i < str.length; i++ ){
+    if( foundDecimal )
+      decimal = decimal + str[i];
+    if( str[i] === "." )
+      if( foundDecimal )
+        decimal = "";
+      foundDecimal = true;
+  }
+
+  return decimal;
 }
 
 /*
